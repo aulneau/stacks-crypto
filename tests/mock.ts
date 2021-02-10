4345a6f6535b0a40496c9d07273cd3875708ae14
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-02-10 12:23:21 -0600

    fix: add mock data

---
+++ tests/mock.ts
@@ -0,0 +1,13 @@
+const legacyData = {
+  data: '{"foo":"hello world","bar":"hello world"}',
+  privateKey: '78883117c95a9fbf618470353c20b1aff5965ab2d62430f7c13016c80a9b388c',
+  publicKey: '03c62fb88f4f5e1969537f7e977553ac4f8991607cdaa0fa9652ec3ec1aace53e6',
+  encrypted: {
+    iv: 'a771bf905f33d0ecb123873e78acd666',
+    ephemeralPK: '026806fdcce267cf4926403647ba71c980bb22b89a20e66ea79cf384858fd34ed0',
+    cipherText:
+      'd37f04f6e7fb4f1336df13d46d4c06a3ad70693f7a0ff87615b3a6c42996cce584e18f30c57aa8172bcf7806e92bac42',
+    mac: 'bc33d5c356e8327808723551982c482291be3641d8d3d4e62eb15d438bed07b9',
+    wasString: true,
+  },
+};
